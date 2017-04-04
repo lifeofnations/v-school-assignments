ab5cdf7a27eb739633bfd4aaf35af9a4ecd62f22
Steven Davis <lifeofnations@msn.com>
2017-04-03 20:00:14 -0600

    finally got it

--- practice/user-authentication/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    database: "mongodb://localhost/todo-ua-test",
-    secret: "this can be what ever you want the more words the better",
-    sendgridApiKey: "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"
-};
